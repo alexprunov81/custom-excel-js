5ec808118995fd463b2e4f4ddc366d709ae921d6
alexprunov81 <87337284+alexprunov81@users.noreply.github.com>
2021-08-05 02:29:50 +0300

    Finish project config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log("hello Excel")
+import './scss/index.scss'
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,87 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-module.exports = {
+module.exports = (env, argv) => {
+  const isPod = argv.mode === 'production'
+  const isDev = !isPod
+
+  const filename = (ext) => isPod ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+  const plugins = () => {
+    const base = [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        scriptLoading: 'blocking',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist'),
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename('css'),
+      }),
+    ]
+    if (isDev) {
+      base.push(new ESLintPlugin())
+    }
+    return base
+  }
+  return {
+    target: 'web',
     context: path.resolve(__dirname, 'src'),
     entry: {
-        main: './index.js'
+      main: [
+        './index.js',
+        'core-js/stable',
+        'regenerator-runtime/runtime',
+      ],
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        clean: true
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename('js'),
+      clean: true,
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            scriptLoading: 'blocking'
-        })
-    ],
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'core'),
+      },
+    },
+    devServer: {
+      port: '8080',
+      // open: true,
+      hot: true,
+    },
+    devtool: isDev ? 'source-map' : false,
+    plugins: plugins(),
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  }
 }
