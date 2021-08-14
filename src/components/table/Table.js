527313c75606d0c78159c2ca3aed414c53d6a5fd
alexprunov81 <87337284+alexprunov81@users.noreply.github.com>
2021-08-14 15:08:48 +0300

    Finish cell-resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
-import {$} from '../../core/dom';
+import {resizeHandler} from '@/components/table/resizeHandler'
+import {shouldResize} from '@/components/table/table.function'
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -17,33 +18,9 @@ export class Table extends ExcelComponent {
       return createTable(20)
     }
 
-    // onClick(event) {
-    //   console.log(event)
-    // }
-    //
     onMousedown(event) {
-      const resize = event.target.dataset.resize
-      if (resize) {
-        const $resizer = $(event.target)
-        const $parent = $resizer.closest('[data-type="resizable"]')
-        const coords = $parent.getCoords()
-
-        document.onmousemove = (e) => {
-          const delta = e.pageX - coords.right
-          const value = coords.width + delta
-          $parent.$el.style.width = value + 'px'
-        }
-        document.onmouseup = () => {
-          document.onmousemove = null
-        }
+      if (shouldResize(event)) {
+        resizeHandler(this.$root, event)
       }
     }
-  //
-  // onMousemove(event) {
-  //   console.log(event)
-  // }
-  //
-  // onMouseup(event) {
-  //   console.log(event)
-  // }
 }
